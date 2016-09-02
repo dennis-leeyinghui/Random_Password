62bc9eea339c88c8ef9f221b062704410c4721bd
dennis-leeyinghui <dennis.leeyinghui@gmail.com>
2016-09-02 21:00:23 +0800

    Random Password
    Generates a High-Level 6 to 8 digits password

---
+++ random_number.py
@@ -0,0 +1,26 @@
+import random
+
+character = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()1234567890"
+print_char = ""
+start = True
+    
+def get_random_no():
+    random_number = random.randrange(0,72)
+    return random_number
+
+while start:
+    
+    for a in range(random.randrange(6,8)):
+        print_char += (character[get_random_no()])
+        get_random_no()
+        
+    print (print_char)    
+    print_char = ""
+    
+    ask_stay = input("Do you want to start again? Y/N  ")
+
+    if (ask_stay == "N" or ask_stay == "n"):
+        print ("BYE BYE")
+        start = False
+    
+#print (random_password)
